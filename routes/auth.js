21d972630b3fe0d0f7ceb820bde760fb3279e6ef
zilleabbas10 <zilleabbas10@gmail.com>
2021-02-24 00:15:11 +0500

    za - added middlewares for auth and admin access

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, signin, signout, requireSignin } = require("../controllers/auth")
+const { signup, signin, signout } = require("../controllers/auth")
 const { userSignupValidator } = require("../validators")
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
-
-// router.get("/hello", requireSignin, (req, res) => {
-//     res.send("Hello route")
-// })
 
 module.exports = router;
