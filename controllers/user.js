25c1dc4be1a5aca8b165d0e586be7029c6348e14
zilleabbas10 <zilleabbas10@gmail.com>
2021-02-23 23:28:55 +0500

    za - created requireSignin method to check authenticated routes

--- controllers/user.js
+++ controllers/user.js
@@ -53,3 +53,9 @@ exports.signout = (req, res) => {
         message: "Logout Successfully!!"
     })
 }
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+    userProperty: "auth"
+})
