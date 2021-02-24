c71b0f2b9179ec7154a20d99a41ee44089ed1136
zilleabbas10 <zilleabbas10@gmail.com>
2021-02-25 01:06:01 +0500

    za - tested create product API in postman.. also applied validations for all product fields and photo size

--- controllers/product.js
+++ controllers/product.js
@@ -15,9 +15,25 @@ exports.create = (req, res) => {
             })
         }
 
+        // validation check for all fields
+        const { name, description, price, category, quantity, shipping } = fields
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: "All product fields are required!!"
+            })
+        }
+
         const product = new Product(fields)
 
         if(files.photo){
+            // validation check for image size
+            // 1kb = 1000
+            // 1mb = 100000
+            if(files.photo.size > 100000){
+                return res.status(400).json({
+                    error: "Image size should not be greater then 1mb... Please try again!!"
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
