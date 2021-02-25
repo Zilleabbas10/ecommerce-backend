ff35fb1b947900aad5aa034a71c8ca1f2522323a
zilleabbas10 <zilleabbas10@gmail.com>
2021-02-26 00:51:40 +0500

    za - added categoryById middleware and read, update, delete, list routes & controller methods for category

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category")
+const { categoryById, create, read, remove, update, list } = require("../controllers/category")
 const { userById} = require("../controllers/user")
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth")
 
+router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/categories', list)
 
 router.param("userId", userById)
+router.param("categoryId", categoryById)
 
 module.exports = router;
